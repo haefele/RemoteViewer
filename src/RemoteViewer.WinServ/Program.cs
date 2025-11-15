03de5032f7b9530e1184299b7ad984ed47981c23
Daniel Häfele <haefele@xemio.net>
2025-11-15 21:55:21 +0100

    Adding serilog for logging to RemoteViewer.WinServ

--- src/RemoteViewer.WinServ/Program.cs
+++ src/RemoteViewer.WinServ/Program.cs
@@ -1,11 +1,37 @@
 using RemoteViewer.WinServ.Options;
 using RemoteViewer.WinServ.Services;
+using Serilog;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddOptions<RemoteViewerOptions>().Bind(builder.Configuration.GetSection("RemoteViewer"));
-builder.Services.AddHostedService<TrackActiveSessionsBackgroundService>();
-builder.Services.AddSingleton<IWin32Service, Win32Service>();
-builder.Services.AddWindowsService();
+try
+{
+    var builder = Host.CreateApplicationBuilder(args);
 
-var host = builder.Build();
-host.Run();
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
+
+    // Options
+    builder.Services.AddOptions<RemoteViewerOptions>().Bind(builder.Configuration.GetSection("RemoteViewer"));
+
+    // Hosted Services
+    builder.Services.AddHostedService<TrackActiveSessionsBackgroundService>();
+
+    // Services
+    builder.Services.AddSingleton<IWin32Service, Win32Service>();
+    builder.Services.AddWindowsService();
+    builder.Services.AddSerilog();
+
+    var host = builder.Build();
+    await host.RunAsync();
+    return 0;
+}
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
