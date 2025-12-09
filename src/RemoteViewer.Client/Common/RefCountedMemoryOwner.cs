bf900fd4dd2a5de26c24c409d05952ee75fa736a
Daniel Häfele <haefele@xemio.net>
2025-12-09 22:30:43 +0100

    Add RefCountedMemoryOwner as a buffer helper for improved memory usage

---
+++ src/RemoteViewer.Client/Common/RefCountedMemoryOwner.cs
@@ -0,0 +1,59 @@
+﻿using System.Buffers;
+
+namespace RemoteViewer.Client.Common;
+
+public sealed class RefCountedMemoryOwner<T> : IMemoryOwner<T>, IDisposable
+{
+    private readonly ArrayPool<T> _pool;
+    private readonly T[] _array;
+
+    private int _refCount = 1;
+    private int _disposed;
+
+    public static RefCountedMemoryOwner<T> Create(int length) => new RefCountedMemoryOwner<T>(ArrayPool<T>.Shared, length);
+    private RefCountedMemoryOwner(ArrayPool<T> pool, int length)
+    {
+        this._pool = pool;
+        this._array = this._pool.Rent(length);
+        this.Length = length;
+    }
+
+    public int Length { get; }
+
+    public Memory<T> Memory
+    {
+        get
+        {
+            ObjectDisposedException.ThrowIf(this._disposed != 0, nameof(RefCountedMemoryOwner<T>));
+            return new Memory<T>(this._array, 0, this.Length);
+        }
+    }
+
+    public Span<T> Span
+    {
+        get
+        {
+            ObjectDisposedException.ThrowIf(this._disposed != 0, nameof(RefCountedMemoryOwner<T>));
+            return new Span<T>(this._array, 0, this.Length);
+        }
+    }
+
+    public RefCountedMemoryOwner<T> AddRef()
+    {
+        ObjectDisposedException.ThrowIf(this._disposed != 0, nameof(RefCountedMemoryOwner<T>));
+
+        Interlocked.Increment(ref this._refCount);
+        return this;
+    }
+
+    public void Dispose()
+    {
+        if (Interlocked.Decrement(ref this._refCount) == 0)
+        {
+            if (Interlocked.Exchange(ref this._disposed, 1) == 0)
+            {
+                this._pool.Return(this._array);
+            }
+        }
+    }
+}
