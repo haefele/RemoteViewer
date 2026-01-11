c7814e4f4e96cc92869310b8af63805d712b92df
Daniel Häfele <haefele@xemio.net>
2026-01-11 19:51:32 +0100

    WIP

--- src/RemoteViewer.Client/Controls/Card.axaml.cs
+++ src/RemoteViewer.Client/Controls/Card.axaml.cs
@@ -3,42 +3,23 @@ using Avalonia.Controls;
 
 namespace RemoteViewer.Client.Controls;
 
-public enum CardVariant
+public enum CardSize
 {
     Default,
-    Elevated,
-    Surface,
-    AccentStrip
-}
-
-public enum CardAccent
-{
-    None,
-    Success,
-    Error,
-    Info,
-    Warning,
-    Primary
+    Large,
+    XLarge,
+    ListItem
 }
 
 public partial class Card : ContentControl
 {
-    public static readonly StyledProperty<CardVariant> VariantProperty =
-        AvaloniaProperty.Register<Card, CardVariant>(nameof(Variant), CardVariant.Default);
+    public static readonly StyledProperty<CardSize> SizeProperty =
+        AvaloniaProperty.Register<Card, CardSize>(nameof(Size), CardSize.Default);
 
-    public static readonly StyledProperty<CardAccent> AccentProperty =
-        AvaloniaProperty.Register<Card, CardAccent>(nameof(Accent), CardAccent.None);
-
-    public CardVariant Variant
+    public CardSize Size
     {
-        get => this.GetValue(VariantProperty);
-        set => this.SetValue(VariantProperty, value);
-    }
-
-    public CardAccent Accent
-    {
-        get => this.GetValue(AccentProperty);
-        set => this.SetValue(AccentProperty, value);
+        get => this.GetValue(SizeProperty);
+        set => this.SetValue(SizeProperty, value);
     }
 
     public Card()
